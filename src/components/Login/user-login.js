7b0981ef646f2b2fddc7ca9a0df488cc5b484510
Peter Sekesan <psekesan@gmail.com>
2021-03-13 16:20:24 -0500

    added services, lost on config file

--- src/components/Login/user-login.js
+++ src/components/Login/user-login.js
@@ -1,20 +1,44 @@
 import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
+import TokenService from "../services/token-service";
+import { API_BASE_URL } from "../config";
 import "./user-login.css";
 
 class UserLogin extends Component {
+  handleLogin = (e) => {
+    e.preventDefault();
+    const { userName, password } = e.target;
+    const newUser = {
+      user: userName.value,
+      password: password.value,
+    };
+    fetch(
+      `${API_BASE_URL}/api/auth/login`, {
+        method: "POST",
+      headers: {
+          'Accept': "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser);
+    }).then(res => res.json()).then(loginResponse => {
+      TokenService.saveAuthToken(loginResponse.authToken)
+      this.props.history.push('/dashboard');
+    }).catch(err =>console.error(err));
+   
+  };
+
   render() {
     return (
       <Fragment>
         <section>
           <div>
             <p>User Login</p>
-            <form>
+            <form className="login-form" onSubmit={this.handleLogin}>
               <div>
                 <label>Username:</label>
-                <input type="text" required />
+                <input type="text" required name="userName" />
                 <label>Password:</label>
-                <input type="password" required />
+                <input type="password" required name="password" />
                 <br />
                 <button type="submit">Log In</button>
                 <br />
