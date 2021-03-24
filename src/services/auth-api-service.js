62d9f80829cd71fa1e74f0127474e62a0e3c467e
Peter Sekesan <psekesan@gmail.com>
2021-03-24 15:49:44 -0400

    post workout/user asdded to auth-api services

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,4 +1,5 @@
 import config from "../config";
+import TokenService from "./token-service";
 
 export default {
   postUser(user) {
@@ -8,6 +9,20 @@ export default {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(user),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+
+  postWorkout(workout) {
+    const token = "bearer" + TokenService.hasAuthToken();
+    return fetch(`${config.API_BASE_URL}/api/workouts`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      body: JSON.stringify(workout),
     }).then((res) =>
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
