938ccf222c86e0a49d000909c323e02972dfb06f
Peter Sekesan <psekesan@gmail.com>
2021-03-25 14:49:33 -0400

    auth api services completed API endpoint to config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   API_BASE_URL: "http://localhost:8000",
+  API_ENDPOINT: "https://mind-your-fitness-api.herokuapp.com/",
   TOKEN_KEY: "fitness-tracker-token",
 };
--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -4,7 +4,7 @@ import TokenService from "./token-service";
 export default {
   postUser(user) {
     return fetch(`${config.API_BASE_URL}/api/users`, {
-      method: "post",
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
@@ -17,12 +17,38 @@ export default {
   postWorkout(workout) {
     const token = "bearer" + TokenService.hasAuthToken();
     return fetch(`${config.API_BASE_URL}/api/workouts`, {
-      method: "post",
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: token,
       },
       body: JSON.stringify(workout),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+
+  getWorkout(workout) {
+    const token = "bearer" + TokenService.hasAuthToken();
+    return fetch(`${config.API_BASE_URL}/api/workouts`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      body: JSON.stringify(workout),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+
+  userLogin(user) {
+    return fetch(`${config.API_ENDPOINT}/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
     }).then((res) =>
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
