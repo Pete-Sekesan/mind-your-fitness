0894e259abb25dd3dbac754d078acdd9375e14cc
Peter Sekesan <psekesan@gmail.com>
2021-04-06 21:01:25 -0400

    dashboard posting

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -5,19 +5,22 @@ import "./dashboard.css";
 
 class Dashboard extends Component {
   static contextType = Context;
+  constructor(props) {
+    super(props);
+    this.state = { workouts: [] };
+  }
 
-  // componentDidMount() {
-  //   AuthAPIService.getWorkout()
-  //     .then((workouts) => {
-  //       this.context.setWorkout(workouts);
-  //     })
-  //     .catch((res) => {
-  //       this.setState({ error: res.error });
-  //     });
-  // }
+  componentDidMount() {
+    AuthAPIService.getWorkout()
+      .then((workouts) => {
+        this.setState({ workouts });
+      })
+      .catch((res) => {
+        this.setState({ error: res.error });
+      });
+  }
 
   render() {
-    const { workouts = [] } = this.context;
     return (
       <section>
         <div>
@@ -30,7 +33,15 @@ class Dashboard extends Component {
                 <th>Date</th>
               </tr>
             </thread>
-            <tbody> Testing</tbody>
+            <tbody>
+              {this.state.workouts.map((workout) => (
+                <tr key={workout.id}>
+                  <td>{workout.workout_name}</td>
+                  <td>{workout.duration}</td>
+                  <td>{workout.date_created}</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </section>
