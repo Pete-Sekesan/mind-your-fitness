11ef585e4c58920fbd681df750ac37da9feea897
Peter Sekesan <psekesan@gmail.com>
2021-04-07 14:08:28 -0400

    added cors anywhere proxy to config

--- src/dist/config.dev.js
+++ src/dist/config.dev.js
@@ -3,6 +3,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   //API_ENDPOINT: "http://localhost:8000",
-  API_ENDPOINT: "https://desolate-reaches-15214.herokuapp.com",
+  API_ENDPOINT: "https://cors-anywhere.herokuapp.com/https://desolate-reaches-15214.herokuapp.com",
   TOKEN_KEY: "fitness-tracker-token"
 };
