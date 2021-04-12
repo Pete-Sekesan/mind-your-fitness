2392b96877deafa8258affc5a0fbbfa1dbb03806
Peter Sekesan <psekesan@gmail.com>
2021-04-12 19:41:20 -0400

    register css done

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -26,29 +26,26 @@ class Register extends Component {
   render() {
     return (
       <Fragment>
-        <section>
-          <div>
-            <p>Register</p>
-            <form onSubmit={this.handleSubmit}>
-              {this.state.error && <p className="error">{this.state.error}</p>}
-              <div>
-                <label>Username:</label>
-                <input type="text" required name="username" id="username" />
-                <label>Password:</label>
-                <input type="password" required name="password" id="password" />
-                <label>Confirm Password:</label>
-                <input
-                  type="password"
-                  required
-                  name="confirmPassword"
-                  id="confirmPassword"
-                />
-                <br />
-                <button type="submit">Sign Up</button>
-              </div>
-            </form>
-          </div>
-        </section>
+        <div className="form">
+          <h2>Register</h2>
+          <form className="form-inline" onSubmit={this.handleSubmit}>
+            {this.state.error && <p className="error">{this.state.error}</p>}
+
+            <label>Username:</label>
+            <input type="text" required name="username" id="username" />
+            <label>Password:</label>
+            <input type="password" required name="password" id="password" />
+            <label>Confirm Password:</label>
+            <input
+              type="password"
+              required
+              name="confirmPassword"
+              id="confirmPassword"
+            />
+            <br />
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
       </Fragment>
     );
   }
