d25e7d3353a5e3eb3fe1cae233188735a5f0ea2c
Peter Sekesan <psekesan@gmail.com>
2021-04-11 21:45:52 -0400

    css b nav and landing

--- src/components/Landing/landing-page.js
+++ src/components/Landing/landing-page.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logos/logo_transparent_background.png";
 
@@ -7,57 +7,60 @@ import "./landing-page.css";
 class LandingPage extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <div className="landing__logo">
           <img src={Logo} alt="logo" />
         </div>
-        <h1>Welcome to Mind Your Fitness</h1>
-        <section className="landing-description">
-          <article className="landing-description__item">
-            <h3>What is Mind Your Fitness?</h3>
-            <br />
-            <p>
-              This app was created to allow users to have a small, standalone
-              application to log and keep track of their exercise's and to view
-              them all in one central location. Setting up an account is easy.
-              Just create a user name and password and you will be able to
-              select from a list of predefined workouts. If you want to take a
-              look around as well, you can log in with our Demo Account to give
-              it a shot.
-            </p>
-          </article>
-          <article className="landing-description__item">
-            <h3>How to use it</h3>
-            <br />
-            <p>
-              This app was created to allow users to have a small, standalone
-              application to log and keep track of their exercise's and to view
-              them all in one central location. Setting up an account is easy.
-              Just create a user name and password and you will be able to
-              select from a list of predefined workouts. If you want to take a
-              look around as well, you can log in with our Demo Account to give
-              it a shot.
-            </p>
-          </article>
-          <article className="landing-description__item">
-            <h3>Coming soon...</h3>
-            <br />
-            <p>
-              This app was created to allow users to have a small, standalone
-              application to log and keep track of their exercise's and to view
-              them all in one central location. Setting up an account is easy.
-              Just create a user name and password and you will be able to
-              select from a list of predefined workouts. If you want to take a
-              look around as well, you can log in with our Demo Account to give
-              it a shot.
-            </p>
-          </article>
+
+        <section className="landing">
+          <h1>Welcome to Mind Your Fitness</h1>
+          <div className="landing-description">
+            <article className="landing-description__item">
+              <h3>What is Mind Your Fitness?</h3>
+              <br />
+              <p>
+                This app was created to allow users to have a small, standalone
+                application to log and keep track of their exercise's and to
+                view them all in one central location. Setting up an account is
+                easy. Just create a user name and password and you will be able
+                to select from a list of predefined workouts. If you want to
+                take a look around as well, you can log in with our Demo Account
+                to give it a shot.
+              </p>
+            </article>
+            <article className="landing-description__item">
+              <h3>How to use it</h3>
+              <br />
+              <p>
+                This app was created to allow users to have a small, standalone
+                application to log and keep track of their exercise's and to
+                view them all in one central location. Setting up an account is
+                easy. Just create a user name and password and you will be able
+                to select from a list of predefined workouts. If you want to
+                take a look around as well, you can log in with our Demo Account
+                to give it a shot.
+              </p>
+            </article>
+            <article className="landing-description__item">
+              <h3>Coming soon...</h3>
+              <br />
+              <p>
+                This app was created to allow users to have a small, standalone
+                application to log and keep track of their exercise's and to
+                view them all in one central location. Setting up an account is
+                easy. Just create a user name and password and you will be able
+                to select from a list of predefined workouts. If you want to
+                take a look around as well, you can log in with our Demo Account
+                to give it a shot.
+              </p>
+            </article>
+          </div>
+          <br />
+          <button className="btn">
+            <Link to="/login">Get Started</Link>
+          </button>
         </section>
-        <br />
-        <button className="btn">
-          <Link to="/login">Get Started</Link>
-        </button>
-      </div>
+      </Fragment>
     );
   }
 }
