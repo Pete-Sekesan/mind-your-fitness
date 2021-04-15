df124d376f0fb21d9e560843a458a81f54249d69
Peter Sekesan <psekesan@gmail.com>
2021-04-15 10:26:18 -0400

    dashboard fix attempt

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import AuthAPIService from "../../services/auth-api-service";
 import "./dashboard.css";
 
@@ -20,26 +20,30 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div className="workout-container">
-        <h2>Logged Workouts</h2>
-        <table className="workout-table">
-          <tr>
-            <th>Workout</th>
-            <th>Duration</th>
-            <th>Date</th>
-          </tr>
+      <Fragment>
+        <div>
+          <h2>Logged Workouts</h2>
+        </div>
+        <div className="workout-container">
+          <table className="workout-table">
+            <tr>
+              <th>Workout</th>
+              <th>Duration</th>
+              <th>Date</th>
+            </tr>
 
-          <tbody>
-            {this.state.workouts.map((workout) => (
-              <tr key={workout.id}>
-                <td>{workout.workout_name}</td>
-                <td>{workout.duration} Minutes</td>
-                <td>{new Date(workout.date_created).toLocaleDateString()}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            <tbody>
+              {this.state.workouts.map((workout) => (
+                <tr key={workout.id}>
+                  <td>{workout.workout_name}</td>
+                  <td>{workout.duration} Minutes</td>
+                  <td>{new Date(workout.date_created).toLocaleDateString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </Fragment>
     );
   }
 }
